d4805858816118eeb5d008f504eaa3e96dec2d51
spectatora <spectatora_91@abv.bg>
2017-12-15 15:27:41 +0200

    updated tests related to array and some math functions

--- test/test-pages.js
+++ test/test-pages.js
@@ -1,23 +1,26 @@
 var expect  = require('chai').expect;
 var request = require('request');
 
-it('Main page content', function(done) {
-    request('http://localhost:8080' , function(error, response, body) {
-        expect(body).to.equal('Hello World');
-        done();
+var assert = require('assert');
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function(){
+      assert.equal(-1, [1,2,3].indexOf(4));
     });
+  });
 });
 
-it('Main page status', function(done) {
-    request('http://localhost:8080' , function(error, response, body) {
-        expect(response.statusCode).to.equal(200);
-        done();
+// Create a test suite (group) called Math
+describe('Math', function() {
+    // Test One: A string explanation of what we're testing
+    it('should test if 3*3 = 9', function(){
+      // Our actual test: 3*3 SHOULD EQUAL 9
+      assert.equal(9, 3*3);
     });
-});
-
-it('About page content', function(done) {
-    request('http://localhost:8080/about' , function(error, response, body) {
-        expect(response.statusCode).to.equal(404);
-        done();
+    // Test Two: A string explanation of what we're testing
+    it('should test if (3-4)*8 = -8', function(){
+      // Our actual test: (3-4)*8 SHOULD EQUAL -8
+      assert.equal(-8, (3-4)*8);
     });
 });
