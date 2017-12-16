6c122943eb5a74d0296b8e7add3adb5783e186e6
spectatora <spectatora_91@abv.bg>
2017-12-16 14:58:09 +0200

    broken test

--- test/test-pages.js
+++ test/test-pages.js
@@ -3,10 +3,12 @@ var request = require('request');
 
 var assert = require('assert');
 
+
+
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(4));
+      assert.equal(-1, [1,2,3,4,5].indexOf(4));
     });
   });
 });
